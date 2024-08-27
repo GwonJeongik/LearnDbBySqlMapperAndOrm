0172f9cad6b48fa61c5ba52d88c78e32c0da2303
ik <jeongik11029@naver.com>
2024-08-27 16:17:07 +0900

    MyBatisItemRepository -> ItemRepository 구현 -> MyBatisConfig 설정 정보로 등록 -> @Import 사용 | 테스트 코드 실행 -> 오류 -> 속성 이름 오입력 -> MaxPrice X, mapPrice 등 -> 이후 테스트 코드 성공 -> 확인 완료

--- src/main/java/hello/itemservice/repository/ItemUpdateDto.java
+++ src/main/java/hello/itemservice/repository/ItemUpdateDto.java
@@ -2,7 +2,6 @@ package hello.itemservice.repository;
 
 import lombok.Data;
 
-@Data
 public class ItemUpdateDto {
     private String itemName;
     private Integer price;
@@ -14,6 +13,30 @@ public class ItemUpdateDto {
     public ItemUpdateDto(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
 }
