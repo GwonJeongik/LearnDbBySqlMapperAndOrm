3d198f89d38ae90dadb39183bd19c49bbcd6add1
ik <jeongik11029@naver.com>
2024-08-30 11:14:49 +0900

    ItemServiceV2 -> ItemRepositoryV2 [SpringDataJPA] && ItemQueryRepositoryV2 [복잡한 동적 쿼리 여기서 처리] -> 최종적으로 다른 구조로 변경해봄

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -21,7 +21,8 @@ import javax.sql.DataSource;
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
 //@Import(SpringDataJpaConfig.class)
-@Import(QueryDslConfig.class)
+//@Import(QueryDslConfig.class)
+@Import(v2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
---
+++ src/main/java/hello/itemservice/config/v2Config.java
@@ -0,0 +1,38 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import hello.itemservice.service.ItemServiceV2;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class v2Config {
+
+    private final EntityManager em;
+    private final ItemRepositoryV2 repositoryV2; //스프링 데이터 JPA
+
+    @Bean
+    public ItemServiceV2 itemService() {
+        return new ItemServiceV2(repositoryV2, itemQueryRepository());
+    }
+
+    @Bean
+    public ItemQueryRepositoryV2 itemQueryRepository() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV3(em);
+    }
+}
